16ec8b8e185fadd4365360f5c065938aac576f30
Ivan <antonio93mx@gmail.com>
2018-04-12 02:54:47 -0500

    urls para las vistas de detalles funcionando

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -9,5 +9,7 @@ def post_list(request):
 
 def post_detail (request, pk):
 	Post.objects.get(pk=pk)
+	post= get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html',{'post':post})
 
 # Create your views here.
