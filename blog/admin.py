4fd716157389da16ec7dd7fe99ff696d88463d71
Ivan <antonio93mx@gmail.com>
2018-05-02 18:19:15 -0500

    se agregan primeras lineas de comentarios para los post

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Post 
+from .models import Post, Comment
 
 
+
+Post.objects.order_by('created_date')
 admin.site.register(Post)
+admin.site.requiter(Comment)
+
 # Register your models here.
